ea30d2590bac50aa2766c98bbcbdd91a4bc35a3e
long lee <long.lee@CZ1CG1.PRI>
2019-08-20 17:57:33 +0700

    Update backend code

---
+++ src/app.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import menus from './assets/data';
+
+@Injectable()
+export class AppService {
+  getMenus(): any {
+    return menus;
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('React Demo Backend API')
+    .setDescription('The React Demo Backend API description')
+    .setVersion('1.0')
+    .setBasePath('api')
+    .addTag('react')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('apidocs', app, document);
+
+  await app.listen(3333);
+}
+bootstrap();
